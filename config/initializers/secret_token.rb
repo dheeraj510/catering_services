41462150db65b4efa7f4afa4a8307d4a5faa8690
sandeep naidu <sandeep.mvl@gmail.com>
2013-08-28 15:45:16 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CateringServices::Application.config.secret_token = '52ba53e4b51b63c31b8361da6fa2d3e245d8e618058845155410aab66932a1516104a35772f55d7b7aaf4bae435251d4d2fa25981476effcac2eec45bac4b76a'
