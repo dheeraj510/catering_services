ab54edfca6cec8a6d4c92a4a692f297b6d6d4d1c
sandeep-panzer <sandeep.m@panzertechnologies.com>
2013-09-10 18:23:31 +0530

    Updated search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   layout 'home'
   def index
     if params[:search]
-    @all_business = Business.search(params[:search])
+    @all_business = Business.search(params[:search]).paginate(:page => params[:page], :per_page => 5)
       end
   end
 
