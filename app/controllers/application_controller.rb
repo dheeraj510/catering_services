73f83ab2b7156fcf9a525f3be2f00e6a85679a17
sandeep-panzer <sandeep.m@panzertechnologies.com>
2013-09-26 03:23:11 +0530

    add js files to change layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :city_select
   private
 
   def city_select
     if cookies[:location].nil?
       redirect_to defaultpage_path
+    else
+      @location = cookies[:location]
     end
   end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   layout 'home'
   before_filter :city_select, :only => [:index]
   def index
-    @location = cookies[:location]
+
     if params[:search]
       @all_business = Business.search(params[:search]).paginate(:page => params[:page], :per_page => 5)
     end
